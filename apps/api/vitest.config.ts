b5423b4cb85c1e2985c87931290a6cce3ae8de9d
GjorgiKrenkov <gjorgikrenkov@icloud.com>
2026-03-24 19:43:59 +0100

    chore: add apps/api/vitest.config.ts

---
+++ apps/api/vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+import { baseVitestConfig } from '@demo-app/config/vitest/base';
+
+export default mergeConfig(baseVitestConfig, defineConfig({
+  test: {
+    environment: 'node',
+    include: ['src/**/*.test.ts'],
+    setupFiles: ['src/__tests__/setup.ts'],
+  },
+}));
