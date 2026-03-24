3d76d259d9af379e2048d84d46ea0c5c6966fd1b
GjorgiKrenkov <gjorgikrenkov@icloud.com>
2026-03-24 19:39:33 +0100

    chore: add lint-staged.config.mjs

---
+++ lint-staged.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('lint-staged').Config} */
+export default {
+  '*.{ts,tsx}': [
+    'eslint --fix --max-warnings=0',
+    'prettier --write',
+  ],
+  '*.{js,jsx,mjs,cjs}': [
+    'prettier --write',
+  ],
+  '*.{json,md,yaml,yml}': [
+    'prettier --write',
+  ],
+};
